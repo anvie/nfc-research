8d276299f2a7a9ef77216395ac2bd3fa9eaacb15
robin <robin@ansvia.com>
2015-05-22 19:18:58 +0700

    bump update

--- hsu2/hsu2.ino
+++ hsu2/hsu2.ino
@@ -18,10 +18,11 @@
 #include <PN532.h>
 #include <PN532_HSU.h>
 #include <NfcAdapter.h>
+#include <PN532Interface.h>
 #include <Ndef.h>
 
 #define UNO                0
-#define IS_DEBUG           0
+#define IS_DEBUG           1
 #define VERSION_CHECKER    0
 #define TEST_TAG           0
 #define TEST_WRITE_TAG     0 // need TEST_TAG
@@ -60,10 +61,12 @@ LiquidCrystal lcd(15, 14, 16, 4, 5, 6, 7);
     PN532_HSU pn532(Serial1);
 #endif
 
-#if !TEST_SNEP
-    NfcAdapter nfc(pn532);
-#else
+#if TEST_SNEP
     SNEP nfc(pn532);
+#elif TEST_HCE
+    PN532 nfc(pn532);
+#else
+    NfcAdapter nfc(pn532);
 #endif
 
 #if VERSION_CHECKER
@@ -121,7 +124,11 @@ void setup(void) {
   #if VERSION_CHECKER
   checkVersion();
   #endif
- 
+  
+  #if TEST_HCE
+    // configure board to read RFID tags
+    nfc.SAMConfig();
+  #endif
   
   #if USING_LCD
   pinMode(2, OUTPUT);
@@ -172,11 +179,15 @@ void loop(void)
   doSNEP();
   #endif
   
+  #if TEST_HCE
+  doHCE();
+  #endif
+  
   delay(1000);
   
 }
 
-#if TEST_SNEP
+#if TEST_SNEP || TEST_HCE
 uint8_t ndefBuf[128];
 bool retry = false;
 
@@ -199,7 +210,9 @@ void clearLcdLine(int num){
   lcd.print(F("                "));
 }
 #endif
+#endif // TEST_SNEP || TEST_HCE
 
+#if TEST_SNEP
 void doSNEP(){
 #if 1
 
@@ -359,6 +372,136 @@ void doSNEP(){
     }
 #endif
 }
+#endif
+
+#if TEST_HCE
+void doHCE(){
+
+  if (isToPay()) {
+    bool success;
+
+    uint8_t response[74];
+    uint8_t responseLength = 32;
+
+    //get payment data from serial
+    char buf[64];
+
+    memset(&buf, 0, 64);
+
+    size_t n = 0;
+
+    for (uint8_t i = 0; Serial.available() > 0; i++){
+      buf[i] = Serial.read();
+      n++;
+    }
+    //DMSG(F("ukurannya\n"));
+    //DMSG(n);
+    //uint8_t buf_i[n];
+    
+    //memcpy(buf_i, buf, n);
+    
+    //display to LCD
+    #if USING_LCD
+    lcd.setCursor(0,0);
+    lcd.print(F("Total (IDR):    "));
+    clearLcdLine(1);
+    lcd.setCursor(0,1);
+    lcd.print("Rp. ");
+    lcd.setCursor(4,1);
+    lcd.print(buf);
+    #endif
+    
+    DMSG(F("Waiting for an Android tap\n"));
+    
+    // set shield to inListPassiveTarget
+    success = nfc.inListPassiveTarget();
+  
+    if(success) {
+     
+       DMSG(F("Found something!\n"));
+                    
+      uint8_t selectApdu[] = { 0x00, /* CLA */
+                                0xA4, /* INS */
+                                0x04, /* P1  */
+                                0x00, /* P2  */
+                                0x07, /* Length of AID  */
+                                0xF0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06//, /* AID defined on Android App */
+                                /*0xF0 // Le  */};
+      uint8_t getDataApdu[] = {0x00, 0xCA, 0x00, 0x00, 
+                               0x00, 
+                               0x00,
+                               0x28};
+       
+      success = nfc.inDataExchange(selectApdu, sizeof(selectApdu), response, &responseLength);
+      
+      if(success) {
+        
+        
+        DMSG(F("Android response #1: ("));
+        DMSG(responseLength);
+        DMSG(") ");
+        nfc.PrintHexChar(response, responseLength);
+        
+        
+        responseLength = 74;
+        memset(response, 0, sizeof(response));
+        
+        success = nfc.inDataExchange((uint8_t*)&buf, strlen(buf), response, &responseLength);
+        
+        DMSG(F("Android response #2: ("));
+        DMSG(responseLength);
+        DMSG(") ");
+        nfc.PrintHexChar(response, responseLength);
+        
+        do {
+  
+          memset(response, 0, sizeof(response));
+          
+          success = nfc.inDataExchange(getDataApdu, sizeof(getDataApdu), response, &responseLength);
+          
+          DMSG(F("Android response #3: ("));
+          DMSG(responseLength);
+          DMSG(") ");
+          nfc.PrintHexChar(response, responseLength);
+
+        
+          if(success && responseLength > 2 && response[0] == '^') {
+            
+            response[strlen((char*)&response)-1] = 0x00;
+            
+            Serial.println("DONE|" + String((const char*)response));
+            
+            #if USING_LCD
+            clearLcdLine(1);
+            lcd.setCursor(0,0);
+            lcd.print(F("PAYMENT SUCCESS "));
+            lcd.setCursor(0,1);
+            lcd.print(F("   THANK YOU    "));
+            #endif
+  
+            DMSG("\nSuccess");
+            //get out from loop
+            success = false;
+          }
+          else {
+            DMSG(F("Broken connection?\n"));
+          }
+        }while(success);
+      }
+      else {
+       
+        DMSG(F("Failed sending SELECT AID\n"));
+      }
+    }
+    else {
+     
+      DMSG(F("Didn't find anything!\n"));
+    }
+  
+    delay(1000);
+  }
+}
+  
 #endif
 
 #if TEST_TAG
